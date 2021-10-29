f4132b772b24db89914455d678a2564aff19a633
Aleksi Tarasov <alexey2g@yandex.ru>
2021-10-30 00:17:39 +0300

    activate cors

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const helmet = require('helmet');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const expressRateLimiter = require('./middlewares/expressRateLimiter');
 const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routes/index');
-const { MONGO_DATEBASE_LINK } = require('./utils/constants');
-// const usersRouter = require('./routes/users');
-// const moviesRouter = require('./routes/movies');
+const { MONGO_DATEBASE_LINK, ALLOWED_CORS_LINKS } = require('./utils/constants');
 
 mongoose.connect(MONGO_DATEBASE_LINK, {
   useNewUrlParser: true,
@@ -23,15 +21,15 @@ mongoose.connect(MONGO_DATEBASE_LINK, {
 });
 const app = express();
 
-// app.use(cors({
-//   origin: ALLOWED_CORS_LINKS,
-// }));
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUTP, PATCH, POST, DELETE, OPTIONS');
-//   next();
-// });
+app.use(cors({
+  origin: ALLOWED_CORS_LINKS,
+}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUTP, PATCH, POST, DELETE, OPTIONS');
+  next();
+});
 
 app.use(express.json());
 app.use(helmet());
