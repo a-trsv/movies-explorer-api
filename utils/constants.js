3198b92e29717a70fff49a341ccabec233b696f7
Aleksi Tarasov <alexey2g@yandex.ru>
2021-10-30 00:02:13 +0300

    testing cors

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,13 @@
 const MONGO_DATEBASE_LINK = 'mongodb://localhost:27017/moviesdb';
 
 const ALLOWED_CORS_LINKS = [
-  'http://a-trsv.nomoredomains.monster',
-  'https://a-trsv.nomoredomains.monster',
-  'https://api.a-trsv.nomoredomains.club',
-  'http://api.a-trsv.nomoredomains.club',
-  'http://84.201.177.135',
-  'localhost:3000',
-  'http://localhost:3000',
+  'http://a-trsv-movies.nomoredomains.work',
+  // 'https://a-trsv-movies.nomoredomains.work',
+  // 'https://api-a-trsv-movies.nomoredomains.work',
+  // 'http://api-a-trsv-movies.nomoredomains.work',
+  // 'http://84.201.176.153',
+  // 'localhost:3000',
+  // 'http://localhost:3000',
 ];
 
 module.exports = {
